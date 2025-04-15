11b44ef5fc7099ef707506974bfa2fc1167c595c
kaitdoch Docherty <kaitlyn.docherty@kellogg.northwestern.edu>
2025-04-15 16:34:05 +0000

    in class exercises

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,35 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone TEXT,
+  role TEXT,
+  company_id INTEGER
+);
+
+CREATE TABLE salesactivities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  type TEXT,
+  date TEXT,
+  time TEXT,
+  note TEXT,
+  contact_id INTEGER,
+  salesrep_id INTEGER
+);
+
+CREATE TABLE salesreps (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
